22dc447f165109af27a408e3d4d7aed33afbd5b4
Leopard0505 <k.9975hyo5@gmail.com>
2020-04-26 16:28:14 +0900

    カードコンポーネントを追加

--- components/molecules/cards/RoundImageCard.vue
+++ components/molecules/cards/RoundImageCard.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="card">
+  <div class="card" @click="edit">
+    <span v-if="update" class="card__status" />
     <img :src="image.src" :alt="image.alt" class="card__image" />
     <p class="card__text">{{ name1 }}</p>
     <p class="card__text">{{ name2 }}</p>
@@ -7,7 +8,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import { Component, Vue, Prop, Emit } from 'nuxt-property-decorator'
 import RoundButton from '../../atoms/buttons/RoundButton.vue'
 
 @Component({
@@ -17,14 +18,27 @@ import RoundButton from '../../atoms/buttons/RoundButton.vue'
   }
 })
 export default class MoleculesCardsCommonCard extends Vue {
+  @Prop({ type: Number }) id!: number
   @Prop({ type: Object }) image!: object
   @Prop({ type: String }) name1!: string
   @Prop({ type: String }) name2!: string
+  @Prop({ type: Boolean }) update!: boolean
+
+  @Emit('edit')
+  private edit() {
+    return {
+      id: this.id,
+      image: this.image,
+      name1: this.name1,
+      name2: this.name2
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .card {
+  position: relative;
   box-sizing: border-box;
   display: inline-block;
   width: 100px;
@@ -33,6 +47,19 @@ export default class MoleculesCardsCommonCard extends Vue {
   background: lighten($primary, 5%);
   border-radius: 5px;
   text-align: center;
+  :hover {
+    cursor: pointer;
+  }
+  &__status {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    width: 10px;
+    height: 10px;
+    background: $red-gradient;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  }
   &__image {
     display: block;
     width: 50px;
--- components/organisms/cards/CommonCard.vue
+++ components/organisms/cards/CommonCard.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="cards">
     <CommonCard @add="open" />
-    <RoundImageCard v-for="data in datas" :key="data.id" v-bind="data" />
+    <RoundImageCard
+      v-for="data in datas"
+      :key="data.id"
+      v-bind="data"
+      @edit="edit"
+    />
+
+    <CommonModal v-if="editModal" class="modal" @close="close">
+      <pre>{{ editData }}</pre>
+      <div class="modal__buttons">
+        <CommonButton class="modal__button" @click="close">
+          閉じる
+        </CommonButton>
+        <CommonButton class="modal__button" type="red-gradient" @click="save">
+          変更
+        </CommonButton>
+      </div>
+    </CommonModal>
 
     <CommonModal v-if="modal" class="modal" @close="close">
       <p>モーダル</p>
@@ -24,161 +41,232 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import dayjs from 'dayjs'
 import CommonCard from '../../molecules/cards/CommonCard.vue'
 import RoundImageCard from '../../molecules/cards/RoundImageCard.vue'
 import CommonModal from '../../molecules/modals/CommonModal.vue'
 import CommonButton from '../../atoms/buttons/CommonButton.vue'
 import SuccessModal from '../../molecules/modals/SuccessModal.vue'
 
-export default {
+interface ICard {
+  id: number
+  image: {
+    src: string
+    alt: string
+  }
+  name1: string
+  name2: string
+  updatedAt: string
+  update: boolean
+}
+
+@Component({
+  name: 'OrganismsCardsCommonCard',
   components: {
     CommonCard,
     RoundImageCard,
     CommonModal,
     CommonButton,
     SuccessModal
-  },
-  data() {
-    return {
-      modal: false,
-      success: false,
-      datas: [
-        {
-          id: 1,
-          image: {
-            src: '/images/atom.svg',
-            alt: 'atom'
-          },
-          name1: 'atom',
-          name2: 'icon'
-        },
-        {
-          id: 2,
-          image: {
-            src: '/images/auth0.svg',
-            alt: 'auth0'
-          },
-          name1: 'auth0',
-          name2: 'icon'
-        },
-        {
-          id: 3,
-          image: {
-            src: '/images/babel.svg',
-            alt: 'babel'
-          },
-          name1: 'babel',
-          name2: 'icon'
-        },
-        {
-          id: 4,
-          image: {
-            src: '/images/branch.svg',
-            alt: 'branch'
-          },
-          name1: 'branch',
-          name2: 'icon'
-        },
-        {
-          id: 5,
-          image: {
-            src: '/images/broccoli.svg',
-            alt: 'broccoli'
-          },
-          name1: 'broccoli',
-          name2: 'icon'
-        },
-        {
-          id: 6,
-          image: {
-            src: '/images/c-plusplus.svg',
-            alt: 'c++'
-          },
-          name1: 'c++',
-          name2: 'icon'
-        },
-        {
-          id: 7,
-          image: {
-            src: '/images/c-sharp.svg',
-            alt: 'c#'
-          },
-          name1: 'c#',
-          name2: 'icon'
-        },
-        {
-          id: 8,
-          image: {
-            src: '/images/c.svg',
-            alt: 'c'
-          },
-          name1: 'c',
-          name2: 'icon'
-        },
-        {
-          id: 9,
-          image: {
-            src: '/images/caffe2.svg',
-            alt: 'caffe2'
-          },
-          name1: 'caffe2',
-          name2: 'icon'
-        },
-        {
-          id: 10,
-          image: {
-            src: '/images/circleci.svg',
-            alt: 'circleci'
-          },
-          name1: 'circleci',
-          name2: 'icon'
-        },
-        {
-          id: 11,
-          image: {
-            src: '/images/css-3.svg',
-            alt: 'css3'
-          },
-          name1: 'css3',
-          name2: 'icon'
-        },
-        {
-          id: 12,
-          image: {
-            src: '/images/dart.svg',
-            alt: 'dart'
-          },
-          name1: 'dart',
-          name2: 'icon'
-        }
-      ]
+  }
+})
+export default class OrganismsCardsCommonCard extends Vue {
+  modal = false
+  editModal = false
+  success = false
+  datas: ICard[] = [
+    {
+      id: 1,
+      image: {
+        src: '/images/atom.svg',
+        alt: 'atom'
+      },
+      name1: 'atom',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    },
+    {
+      id: 2,
+      image: {
+        src: '/images/auth0.svg',
+        alt: 'auth0'
+      },
+      name1: 'auth0',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    },
+    {
+      id: 3,
+      image: {
+        src: '/images/babel.svg',
+        alt: 'babel'
+      },
+      name1: 'babel',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    },
+    {
+      id: 4,
+      image: {
+        src: '/images/branch.svg',
+        alt: 'branch'
+      },
+      name1: 'branch',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    },
+    {
+      id: 5,
+      image: {
+        src: '/images/broccoli.svg',
+        alt: 'broccoli'
+      },
+      name1: 'broccoli',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    },
+    {
+      id: 6,
+      image: {
+        src: '/images/c-plusplus.svg',
+        alt: 'c++'
+      },
+      name1: 'c++',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    },
+    {
+      id: 7,
+      image: {
+        src: '/images/c-sharp.svg',
+        alt: 'c#'
+      },
+      name1: 'c#',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    },
+    {
+      id: 8,
+      image: {
+        src: '/images/c.svg',
+        alt: 'c'
+      },
+      name1: 'c',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    },
+    {
+      id: 9,
+      image: {
+        src: '/images/caffe2.svg',
+        alt: 'caffe2'
+      },
+      name1: 'caffe2',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    },
+    {
+      id: 10,
+      image: {
+        src: '/images/circleci.svg',
+        alt: 'circleci'
+      },
+      name1: 'circleci',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    },
+    {
+      id: 11,
+      image: {
+        src: '/images/css-3.svg',
+        alt: 'css3'
+      },
+      name1: 'css3',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    },
+    {
+      id: 12,
+      image: {
+        src: '/images/dart.svg',
+        alt: 'dart'
+      },
+      name1: 'dart',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
     }
-  },
-  methods: {
-    open() {
-      this.modal = true
-    },
-    complete() {
-      this.success = true
-    },
-    close() {
-      this.modal = false
-      this.success = false
-    },
-    add() {
-      this.datas.unshift({
-        id: this.datas.length + 1,
-        image: {
-          src: '/images/atom.svg',
-          alt: 'atom'
-        },
-        name1: 'atom',
-        name2: 'icon'
-      })
-      this.close()
-      this.complete()
-    }
+  ]
+
+  editData: Partial<ICard> = {}
+
+  get cards(): ICard[] {
+    return this.datas.sort((a, b) => {
+      if (a.updatedAt > b.updatedAt) {
+        return 1
+      } else {
+        return -1
+      }
+    })
+  }
+
+  open() {
+    this.modal = true
+  }
+
+  complete() {
+    this.success = true
+  }
+
+  close() {
+    this.modal = false
+    this.editModal = false
+    this.success = false
+  }
+
+  add() {
+    this.datas.unshift({
+      id: this.datas.length + 1,
+      image: {
+        src: '/images/atom.svg',
+        alt: 'atom'
+      },
+      name1: 'atom',
+      name2: 'icon',
+      updatedAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+      update: true
+    })
+    this.close()
+    this.complete()
+  }
+
+  edit(card: ICard) {
+    this.datas.find((data) => data.id === card.id)
+    this.datas.splice(card.id - 1, 1, { ...card, update: false })
+    this.editData = { ...card, update: false }
+    this.editModal = true
+  }
+
+  save() {
+    const card = this.datas.find((data) => data.id === this.editData.id)
+    if (!card) return
+    card.updatedAt = dayjs().format('YYYY-MM-DD HH:mm:ss')
+    card.update = true
+    this.close()
+    this.complete()
   }
 }
 </script>
